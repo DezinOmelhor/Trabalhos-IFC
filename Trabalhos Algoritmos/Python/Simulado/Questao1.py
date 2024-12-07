4174ea2f4f3752b0f45a68b251b0b918d049cb3a
DezinOmelhor <landreconradtl@gmail.com>
2024-12-07 00:08:24 -0300

    Update Questao1.py

--- Trabalhos Algoritmos/Python/Simulado/Questao1.py
+++ Trabalhos Algoritmos/Python/Simulado/Questao1.py
@@ -1,10 +1,5 @@
-n = int(input("Quantos números deseja informar? "))  # Número total de entradas
-a = 0  # Variável para acumular a soma dos resultados
-m = 1  # Fator multiplicador inicial
-
-for _ in range(n):  # Itera 'n' vezes
-    numero = int(input("Digite um número: "))  # Lê o número
-    a += numero * m  # Multiplica o número pelo fator atual e soma ao total
-    m += 1  # Incrementa o multiplicador
-
-print("Soma dos cálculos:", a)  # Exibe o resultado
+string = input("Digite uma string: ")   # Pede uma string ao usuário
+inverso = ""                            # Cria uma variável para armazenar a string invertida
+for i in range(len(string) -1, -1, -1):    # Passa a string de trás para frente
+    inverso += string[i]
+print(inverso)                               # Mostra o inverso da string
